a3860e4caa0b9ed82cac40f439e51636cc372e42
srpeddi <srpeddi@uwaterloo.ca>
2024-07-26 16:42:11 -0400

    strategy classes

--- Strategy.h
+++ Strategy.h
@@ -1,11 +1,13 @@
+#include "Colour.h"
 #ifndef STRATEGY_H
 #define STRATEGY_H
 
 class Move;
+class Board;
 
 class Strategy {
 private:
-    virtual Move getStrategyImpl() const = 0;
+    virtual Move getStrategyImpl(const Board&, Colour) const = 0;
 
 public:
     Strategy();
