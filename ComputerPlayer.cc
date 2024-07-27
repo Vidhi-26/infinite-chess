85de831134afa6ab9bdf1e2e22bd1161af3435ac
vruparel <vruparel@uwaterloo.ca>
2024-07-26 20:32:36 -0400

    minor changes

--- ComputerPlayer.cc
+++ ComputerPlayer.cc
@@ -4,5 +4,5 @@
 ComputerPlayer::ComputerPlayer(Colour colour, Board& board, Strategy& strategy) : Player{colour, board}, strategy{strategy} {}
 
 Move ComputerPlayer::getMove(const Move& move) const {
-    return strategy.getStrategy();
+    return strategy.getStrategy(board, colour);
 }
---
+++ Level1.cc
@@ -0,0 +1,7 @@
+#include "Level1.h"
+
+Level1::Level1() {}
+
+Move Level1::getStrategyImpl(const Board& board, Colour colour) const {
+    
+}
