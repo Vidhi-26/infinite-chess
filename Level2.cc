73c21cd5bb93a948287d608f4cfdb589cc287bbb
srpeddi <srpeddi@uwaterloo.ca>
2024-07-26 20:28:13 -0400

    added level 2 strategy logic

--- Level2.cc
+++ Level2.cc
@@ -6,18 +6,34 @@
 #include <unordered_map>
 
 Move Level2::getStrategyImpl(const Board& board, Colour colour) const{
-    Move bestMove{};
-    for(auto it = board.cbegin(); it!= board.cend(); ++it){
+    //Keep track of the best move (categorized by most points acquired by capturing)
+    Move bestMove{}; 
+    int bestScore = -1;
+
+    //Iterate over all the board pieces and check which enemy to capture
+    for(auto it = board.cbegin(); it != board.cend(); ++it){
         const Piece& piece = *it;
+        if(piece.getColour() != colour) continue;
         std::vector<Move> moves = piece.getPossibleMoves();
-        
+
+        //Iterate through each possible move of the current piece
         for(auto& move: moves){
+            
+            //Check if there is a piece
+            if(board.isEmptyPosition(move.newPos.first, move.newPos.second)) continue;
             Piece& enemy= board.getPieceAt(move.newPos.first, move.newPos.second);
-            if(enemy.getColour() != colour && enemy.getPoints() > bestMove.second){
-                bestMove = {&move, enemy.getPoints()};
+            
+            //Check if it is an enemy piece and if it has more points
+            if(enemy.getColour() != colour && enemy.getPoints() > bestScore){
+
+                //Update best move
+                bestMove = move;
+                bestScore = enemy.getPoints();
             }
         }
     }
-    if(!bestMove.first) return Level1::getStrategyImpl(board, colour);
-    return *bestMove.first;
+
+    //If no move found that can capture, then use level 1 strategy
+    if(bestMove.newPos.first == -1 || bestMove.newPos.second == -1) return Level1::getStrategyImpl(board, colour);
+    return bestMove;
 }
